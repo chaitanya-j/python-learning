e3a7e268f98c5037a4a335012196114c4dc54fca
Chaitanya Jaipukar <nirmalakulkarni1312@gmail.com>
2020-08-25 15:54:23 +0530

    My first program on Github

---
+++ dict_demo.py
@@ -0,0 +1,24 @@
+# Our Dictionary.
+marks = {
+    "Chaitanya": 98.5,
+    "Neel": 95,
+    "Samarth": 93
+}
+
+# Reading an entry in in marks using square bracket notation.
+ch_marks = marks["Chaitanya"]
+
+# reading an entry in marks using get method.
+neel_marks = marks.get("Neel")
+pan_marks = marks.get("Pankaj")
+
+# Printing the values of.
+print(f"Chaitanya scored {ch_marks} %")
+print(f"Neel scored {neel_marks} %")
+print(f"Pankaj scored {pan_marks} %")
+
+# Adding a new entry in the dictionary. 
+marks["Pankaj"] = 96
+
+# Printing the whole dictionary.
+print(marks)
