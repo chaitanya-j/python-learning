e9ca52c338b396d87ac82b5a8d53a56b8452e51f
Chaitanya Jaipukar <nirmalakulkarni1312@gmail.com>
2020-08-25 16:00:23 +0530

    Added a date demo program

---
+++ date_demo.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+# Create a variable with current date and time
+dt = datetime.today()
+print(dt)
+
+# Create a date as per your requirement
+chaitanya_dob = datetime(2007, 12, 22, 23, 00, 00)
+print(chaitanya_dob)
