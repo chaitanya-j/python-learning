3c289b5a6267dad01071f698284a7b9dd83c26df
Chaitanya Jaipukar <nirmalakulkarni1312@gmail.com>
2020-08-26 20:16:12 +0530

    new add-ons

---
+++ csv_reader.py
@@ -0,0 +1,8 @@
+import csv
+
+with open("example.csv") as csv_file:
+    csv_read = csv.DictReader(csv_file)
+
+    for l in csv_read:
+        print(l)
+        
---
+++ test_dict.py
@@ -0,0 +1,12 @@
+d = {
+    "roll_no":" 98",  
+    "Name":"Chaitanya",
+    "Marks":"  99"
+}
+d["roll_no"] = int(d.get("roll_no"))
+d["roll_no"]
+
+d["Marks"] = int(d.get("Marks"))
+d["Marks"]
+
+print(d)
