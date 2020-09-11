b1c85d453bd09b36002640cc62de09aa672c5113
Chaitanya Jaipurkar <nirmalakulkarni1312@gmail.com>
2020-09-11 18:52:24 +0530

    changes

--- loop101.py
+++ loop101.py
@@ -6,8 +6,8 @@ for i in range(len(arr2)):
     print(i)
 
 usr = int(input("Please enter the no you want to remove:"))
-o = len(arr2)
-for m in range(o):
+c = len(arr2)
+for m in range(c):
     if arr2[m] == usr:
         arr2.pop(m)
         break
