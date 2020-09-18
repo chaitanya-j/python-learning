9788e3489f7e2c86447e947b49f62cf4757eb9d3
Chaitanya Jaipurkar <nirmalakulkarni1312@gmail.com>
2020-09-18 18:41:46 +0530

    added some more spl characters

--- chk_password.py
+++ chk_password.py
@@ -14,7 +14,7 @@ flg_passwd_length = False
 # Thus making it easy to check if the password has all required values
 small_letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 big_letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-spl_characters = ['~','!','@','#','$','%','^','*','&','[',']','(',')','{','}',',',':',';','_','-']
+spl_characters = ['~','!','@','#','$','%','^','*','&','[',']','(',')','{','}',',',':',';','_','-','.','"',"'"]
 nos = ['0','1','2','3','4','5','6','7','8','9']
 
 # Checking Password length
