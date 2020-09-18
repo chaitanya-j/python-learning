e1ad6a7e2ad8fbd1ce26136b68b16285c6f51ab7
Chaitanya Jaipurkar <nirmalakulkarni1312@gmail.com>
2020-09-18 18:15:47 +0530

    changes

--- chk_password.py
+++ chk_password.py
@@ -2,10 +2,7 @@
 passw = input('Please enter your desired passsword:')
 
 # Putting Flags so it will be easy to check
-flg_s_letters = False
-flg_b_letters = False
-flg_spl_characters = False
-flg_chk_no = False
+
 flg_passwd_length = False
 
 
@@ -14,7 +11,7 @@ flg_passwd_length = False
 # Thus making it easy to check if the password has all required values
 small_letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 big_letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-spl_characters = ['~','!','@','#','$','%','^','*','&']
+spl_characters = ['~','!','@','#','$','%','^','*','&','[',']','(',')','{','}',',',':',';','_','-']
 nos = ['0','1','2','3','4','5','6','7','8','9']
 
 # Checking Password length
@@ -24,43 +21,48 @@ if len(passw) >= 9:
 # Using the function list() to seperate every character in the password, making it easy check if the password has all required values
 passwd_chars = list(passw)
 
+cnt_sl = 0
+cnt_cl = 0
+cnt_spl_chars = 0
+cnt_nums = 0
+
 # Checking each and every character of list passwd_chars to see if it has all the required values
 for pass_ch in passwd_chars:
     if pass_ch in small_letters:
-        flg_s_letters = True
+        cnt_sl += 1
 
     if pass_ch in big_letters:
-        flg_b_letters = True
+        cnt_cl += 1
 
     if pass_ch in spl_characters:
-        flg_spl_characters = True
+        cnt_spl_chars += 1
 
     if pass_ch in nos:
-        flg_chk_no = True
-
+        cnt_nums += 1
 
 # Now checking if any flag is still False
 # If its False we print that a type of value is missing 
-if len(passw) < 9:
+if flg_passwd_length == False:
     print('Password must have more than 9 characters! Please try again!')
 
-if flg_s_letters == True and flg_b_letters == True and flg_spl_characters == True and flg_chk_no == True and flg_passwd_length == True:
-    print('You have set a perfect password')
+if cnt_sl >= 2 and cnt_cl >= 2 and cnt_spl_chars >= 2 and cnt_nums >= 2:
+    if flg_passwd_length == True:
+        print('You have set a perfect password')
 
-if flg_s_letters == False:
+if cnt_sl < 2:
     print('Password must have at least one Small Alphabet!')
 
-if flg_b_letters == False:
-    print('Password must have at least one Big Alphabet!')
+if cnt_cl < 2:
+    print('Password must have at least one Capital Alphabet!')
 
-if flg_spl_characters == False:
+if cnt_spl_chars < 2:
     print('Password must have at least one Special Character!')
 
-if flg_chk_no == False:
-    print('Password must hve at least one Numerical Value!')
+if cnt_nums < 2:
+    print('Password must have at least one Numerical Value!')
 
 
-
+  
 
 
 
