12222462219e5babdcb34b8e2d6c36cbd53f8d3e
Chaitanya Jaipurkar <nirmalakulkarni1312@gmail.com>
2020-10-06 15:17:23 +0530

    Just did a program on pickle

---
+++ dict-app/pickle101.py
@@ -0,0 +1,28 @@
+import pickle
+
+# Creating a dict of info of students
+dict_info = {
+    'Name' : 'Chaitanya',
+    'Age' : 12,
+    'Gender' : 'Male',
+    'School' : 'AVEMPS',
+    'Freinds' : ['Adwait','Vishrut','Samartha','Neel'],
+    'Class' : 7
+}
+
+# Opening a new pkl file and dumping the dict dictionary in the pkl file, in write binary mode
+with open('test.pkl','wb') as pkl:
+    pickle.dump(dict_info,pkl)
+    
+# Deleting the dict
+del dict_info
+
+# Now there is no dict named dict_info, so let's revive it and read it, in read binary mode
+with open('test.pkl','rb') as open_pkl:
+    dict_revive = pickle.load(open_pkl)
+
+print(dict_revive)
+
+
+
+    
