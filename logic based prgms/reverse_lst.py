ccf7719b9c0631399a32ff2c3a95862e8793aeec
Chaitanya Jaipurkar <nirmalakulkarni1312@gmail.com>
2020-10-23 09:50:24 +0530

    Added Comments

--- logic based prgms/reverse_lst.py
+++ logic based prgms/reverse_lst.py
@@ -1,10 +1,18 @@
 #lst = [12,13,14,15,16,17,18,20]
+
+
 lst = ['Jhon','Ben','Bob','Chris','Justin','Chaitanya','Adwait']
 
+# Starting a for loop and passing half of the length of the list to the range function
 for i in range(int(len(lst)/2)):
+
+    # Here we are accesing the i'th position in the list
     rev1 = lst[i]
+
+    # Here we are accesing the (i + 1) * (-1) position, which is from the opposite side of the list.
     rev2 = lst[(i + 1) * (-1)]
 
+    # Here we are changing the values in the list 
     lst[i] = rev2
     lst[(i + 1) * (-1)] = rev1
 
